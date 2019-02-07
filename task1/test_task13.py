3a92c0626790a73e6dd63f2f6da5b68ad32cabcf
Mingfeng Li <keithlimingfeng@gmail.com>
2019-02-06 23:09:44 -0500

    update task13

--- task1/test_task13.py
+++ task1/test_task13.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #file_path = 'input.txt'
-file_path = "/home/travis/build/aml-spring-19/homework-1-MingfengKeith/task1/input.txt"
+file_path = "/home/travis/build/MingfengKeith/myrepo/task1/input.txt"
 
 data = pd.read_csv(file_path,encoding='utf-16', header=0, na_values=["--","NA"], escapechar = '\\',index_col=0, error_bad_lines = False)
 
